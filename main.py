6267a71541717c36f7670ba249e227e30ff29470
bobbyhadz <opmftw@gmail.com>
2024-04-09 15:49:22 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Convert an Object to a String in Python
+
+my_int = 42
+
+# ✅ Convert an object to a string
+result = str(my_int)
+print(result)  # 👉️ '42'
+
+print(type(result))  # 👉️ <class 'str'>
+print(isinstance(result, str))  # 👉️ True
